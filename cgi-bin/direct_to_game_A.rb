1eb96199d2af2d17a8a78d0c0a1c5f2d866aa599
amizota <amizota@comp.is.uec.ac.jp>
2015-08-25 10:54:50 +0900

    direct_to_game_A.rb, direct_to_game_Q.rb 追加

---
+++ cgi-bin/direct_to_game_A.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/ruby
+# -*- coding: utf-8 -*-                                                         
+#                                                                               
+require 'cgi'
+require 'cgi/session'
+
+print "Location: http://61.21.43.96/base_A.html\n\n"
+exit
---
+++ cgi-bin/direct_to_game_Q.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/ruby
+# -*- coding: utf-8 -*- 
+
+#
+require 'cgi'
+require 'cgi/session'
+
+print "Location: http://61.21.43.96/base_Q.html\n\n"
+exit                       
